f3877a3b665a86762c736730b6b20b4eccbe827b
Carlos Trujillo <augusto21_20@hotmail.com>
2023-08-04 02:13:47 -0500

    agregando comentarios

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
-
 const cors = require('cors');
+
+const app = express();
 const port = process.env.PORT;
 
+// CORS
 app.use(cors());
+
+// Middleware para el directorio público
 
 // Otro middleware: lectura y parseo del body
 app.use(express.json());
 
+// http://localhost:8080/api/users: Es el path que me permitirá acceder a las rutas
 app.use('/api/users', require('./routes/user'));
 
 app.listen(port);
